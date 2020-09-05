805a780a0d6e10d39ba7668b239dacae8c5027e4
Iagor Sousa <54728248+IagorSs@users.noreply.github.com>
2020-09-05 15:15:56 -0300

    CustomRequest implementted

--- src/controllers/middlewares/auth.ts
+++ src/controllers/middlewares/auth.ts
@@ -3,14 +3,13 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import authConfig from '../../config/auth';
 
-declare namespace Express {
-  export interface Request {
-      token?: string;
-  }
+export interface CustomRequest extends Request{
+  token?: string,
+  user?: object,
 }
 
 // eslint-disable-next-line consistent-return
-function authenticate(req:Request, res:Response, next:NextFunction) {
+function authenticate(req:CustomRequest, res:Response, next:NextFunction) {
   const { token } = req;
   if (!token) {
     return res.status(204).json({ error: 'token can\'t be null' });
