656b52f39c88f76cb29dd18e1da2a9e598c4350a
Lopes <57339312+Andre1999Lopes@users.noreply.github.com>
2020-09-05 11:48:55 -0300

    Implementação de um middleware para autenticação

--- src/controllers/middlewares/auth.ts
+++ src/controllers/middlewares/auth.ts
@@ -1,5 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-
 import authConfig from '../../config/auth';
 
-console.log(authConfig);
+declare namespace Express {
+  export interface Request {
+      token?: string;
+  }
+}
+
+// eslint-disable-next-line consistent-return
+function authenticate(req:Request, res:Response, next:NextFunction) {
+  const { token } = req;
+  if (!token) {
+    return res.status(204).json({ error: 'token can\'t be null' });
+  }
+
+  // eslint-disable-next-line consistent-return
+  jwt.verify(token, authConfig.secret, (err, user) => {
+    if (err) {
+      return res.status(401).json({ error: 'error while authenticating the token' });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+export default authenticate;
