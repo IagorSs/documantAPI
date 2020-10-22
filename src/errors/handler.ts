351595b791f1893ecf479e0aab7463a3811c4ff4
Iagor Sousa <54728248+IagorSs@users.noreply.github.com>
2020-10-22 00:13:28 -0300

    starting errorHandler treatment

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,5 +1,14 @@
+import { AWSError } from 'aws-sdk';
 import { ErrorRequestHandler } from 'express';
 
-const errorHandler:ErrorRequestHandler = (error, req, res, next) => {
-
+const errorHandler:ErrorRequestHandler = (error, req, res) => {
+  switch (error) {
+    case AWSError:
+      res.status(error.statusCode).json({ error: error.message });
+      break;
+    default:
+      res.status(500).json({ error: "Erro inesperado do servidor" });
+  }
 };
+
+export default errorHandler;
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, {
-  json, Request, Response, NextFunction,
+  json,
 } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import AWSConfig from './config/AWS';
 import corsOptions from './config/cors';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -21,9 +22,7 @@ app.use(json());
 app.use(routes);
 app.use(morgan('dev'));
 
-app.use((err:any, req:Request, res:Response, next: NextFunction) => {
-  res.status(err.statusCode || 500).json({ error: err.message });
-});
+app.use(errorHandler);
 
 const port = process.env.LOCAL_PORT || 3002;
 
